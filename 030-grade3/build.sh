254b3c80db47c6a1dd3b7e00741481952dba01ac
Skylar Saveland <skylar.saveland@gmail.com>
2025-02-12 03:49:22 +0000

    rename third-grade-math.pdf

--- 030-grade3/build.sh
+++ 030-grade3/build.sh
@@ -1,4 +1,4 @@
-pandoc -s *.md -o output.pdf --pdf-engine=xelatex --toc \
+pandoc -s *.md -o third-grade-math.pdf --pdf-engine=xelatex --toc \
     --include-in-header=custom_headings.tex \
     -V geometry:margin=1in \
     --toc-depth=3
