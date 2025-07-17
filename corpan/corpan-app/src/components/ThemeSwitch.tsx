cc033fa1a10839bddf9dc6886e575d4407613bb3
Kevin Bravo <bravokevinto@gmail.com>
2025-07-17 16:56:17 -0400

    fix: show up icon

--- corpan/corpan-app/src/components/ThemeSwitch.tsx
+++ corpan/corpan-app/src/components/ThemeSwitch.tsx
@@ -2,7 +2,7 @@ import { Moon, Sun } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useTheme } from "./ThemeProvider";
 
-const ThemeToggle =() => {
+const ThemeToggle = () => {
   const { setTheme, theme } = useTheme();
 
   const toggleTheme = () => {
@@ -11,14 +11,14 @@ const ThemeToggle =() => {
   };
 
   return (
-    <Button size='icon'  onClick={toggleTheme} className="rounded-full">
+    <Button size="icon" onClick={toggleTheme} className="rounded-full">
       {theme === "dark" ? (
-        <Sun className=" scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-      ) : (
         <Moon className="scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      ) : (
+        <Sun className=" scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
       )}
     </Button>
   );
-}
+};
 
-export default  ThemeToggle
+export default ThemeToggle;
