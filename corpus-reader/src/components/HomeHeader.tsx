acc32e84d857cbdf4972f7d3dc7ae62bf6aa0b37
Kevin Bravo <bravokevinto@gmail.com>
2025-07-31 12:15:43 -0400

    style: improve home header style

--- corpus-reader/src/components/HomeHeader.tsx
+++ corpus-reader/src/components/HomeHeader.tsx
@@ -15,17 +15,15 @@ const HomeHeader = ({
 }) => {
   return (
     <div className=" bg-background/60 backdrop-blur-sm border-b sticky top-0 z-50">
-      <div className="flex h-16 items-center justify-between  container px-4 mx-auto max-w-7xl">
-        <div className="flex items-center justify-center ">
-          <Avatar className="mr-2 h-10 w-10">
+      <div className="flex h-14 items-center justify-between  container px-4 mx-auto max-w-7xl">
+        <div className="flex items-center justify-center gap-2 ">
+          <Avatar className="h-10 w-10">
             <AvatarImage src="/logo.png" />
             <AvatarFallback>Logo</AvatarFallback>
           </Avatar>
-          <h1 className="text-xl font-bold">Corporium</h1>
+          <h1 className="md:text-xl font-bold">Corporium</h1>
         </div>
-        <div className="hidden md:block">
-          {/* TODO: add bookmarks and notes section */}
-        </div>
+        <div>{/* TODO: add bookmarks and notes section */}</div>
         <div className="flex items-center space-x-2">
           {/* Search Dialog with debounce */}
           <BookSearchDialog books={books} />
@@ -33,7 +31,7 @@ const HomeHeader = ({
           <Button
             onClick={handleAddBookToLibrary}
             size="sm"
-            className="h-8 w-auto flex items-center justify-center"
+            className="w-auto flex items-center justify-center"
           >
             <PlusIcon className="h-4 w-4" />
             <span className="hidden md:inline ml-1">Add book</span>
