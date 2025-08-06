de049469712094518bdbf6a8f1344671892775d1
Miklos Daniel <daniel.miklos0@gmail.com>
2025-08-06 20:15:02 +0200

    i18n debug only when not prod

--- corpan/corpan-app/src/i18n.ts
+++ corpan/corpan-app/src/i18n.ts
@@ -13,7 +13,7 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: "en", // use 'en' if the user's language is not available
-    debug: true,
+    debug: !import.meta.env.PROD,
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
