c0b9f5408914bd50773dbe2f44733ee54e4360f9
Miklos Daniel <daniel.miklos0@gmail.com>
2025-08-07 23:11:08 +0200

    sync i18n lang with persist

---
+++ corpan/corpan-app/src/components/LanguageSynchronizer.tsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { useSettingsStore } from "@/store/settings";
+
+const LanguageSynchronizer = ({ children }: { children: React.ReactNode }) => {
+  const { i18n } = useTranslation();
+  // Get the current language from the Zustand store
+  const language = useSettingsStore((state) => state.topLang);
+
+  // This useEffect will run whenever the 'language' in the store changes.
+  useEffect(() => {
+    const val = language();
+    console.log("change language", val);
+    i18n.changeLanguage(val);
+  }, [language, i18n]);
+
+  // Optional: Listen for external i18n changes to update the store
+  useEffect(() => {
+    const handleLanguageChange = (lng: string) => {
+      useSettingsStore.getState().setLanguages([lng]);
+    };
+
+    i18n.on("languageChanged", handleLanguageChange);
+
+    return () => {
+      i18n.off("languageChanged", handleLanguageChange);
+    };
+  }, [i18n]);
+
+  return children;
+};
+
+export default LanguageSynchronizer;
