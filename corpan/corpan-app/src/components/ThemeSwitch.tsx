3fa0d829f1ddacc29f7fa0aff7e9a823658be9bf
Kevin Bravo <bravokevinto@gmail.com>
2025-10-06 18:36:54 -0400

    feat: add locales for theme switching

--- corpan/corpan-app/src/components/ThemeSwitch.tsx
+++ corpan/corpan-app/src/components/ThemeSwitch.tsx
@@ -1,8 +1,10 @@
 import { useTheme } from "./ThemeProvider";
 import { Switch } from "./ui/switch";
+import { useTranslation } from "react-i18next";
 
 const ThemeToggle = () => {
   const { setTheme, theme } = useTheme();
+  const { t } = useTranslation();
 
   const toggleTheme = () => {
     if (theme === "dark") setTheme("light");
@@ -11,8 +13,10 @@ const ThemeToggle = () => {
 
   return (
     <div className="flex items-center justify-between space-x-2">
-      <p className="sr-only">Toggle theme</p>
-      <p className="font-medium text-sm">Turn on {theme === "dark" ? "light" : "dark"} mode</p>
+      <p className="sr-only">{t("settings.themeMode")}</p>
+      <p className="font-medium text-sm">
+        {theme === "dark" ? t("settings.turnOnLightMode") : t("settings.turnOnDarkMode")}
+      </p>
       <Switch
         id="toggle-theme"
         checked={theme === "dark"}
