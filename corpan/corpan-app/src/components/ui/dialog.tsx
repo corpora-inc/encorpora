1fc095f916c6dcc9b9755280075ce37bfcd54946
Skylar Saveland <skylar.saveland@gmail.com>
2025-10-24 21:48:31 -0400

    0.7.8, 0.7.9; 16kb page sizes, gen/android hacks, better onboard TTS (#104)
    * 0.7.8 16kb page sizes, belt and suspenders ... some gen/android hacking ;/
    
    * try again
    
    * no workspace right now ..
    
    * breadcrumbs
    
    * some things I changed in gen/android
    
    * refining voice picking installing on android
    
    * revolutionize android onboarding?
    
    * 0.7.9
    
    * two empties
    
    * cleanup
    
    * bengali then

--- corpan/corpan-app/src/components/ui/dialog.tsx
+++ corpan/corpan-app/src/components/ui/dialog.tsx
@@ -71,7 +71,7 @@ function DialogContent({
         }}
       >
         {/* Sticky wrapper reserves space; button is absolute so rings don’t affect layout */}
-        <div className="sticky top-3 z-[1001]">
+        <div className="sticky top-5 z-[1001]">
           <div className="relative h-10">
             <DialogPrimitive.Close
               className="absolute right-0 top-0 inline-flex h-10 w-12 items-center justify-center rounded-md border bg-background shadow-sm cursor-pointer transition-shadow hover:shadow-md focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none pointer-events-auto"
